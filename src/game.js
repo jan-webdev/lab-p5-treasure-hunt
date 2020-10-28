b7275a3801d8129e8ac061126abad798a8850757
Jan <jan.leon.voelker@gmail.com>
2020-10-28 17:01:57 +0100

    First iteration done: the board is live :)

--- src/game.js
+++ src/game.js
@@ -2,6 +2,10 @@ class Game {
   drawGrid() {
     // Iteration 1
     // Draw the grid
-    // https://p5js.org/reference/#/p5/line
+    https://p5js.org/reference/#/p5/line
+    for (let i = 0; i<=500; i+=50) {
+      line(0,0+i,500, 0+i);
+      line(0+i,0,0+i,500);
+    }
   }
 }
